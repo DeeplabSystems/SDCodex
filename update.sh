a937fe7b426cf4dc88ae8110195558b2248573a2
nakedlittlezombie <50752495+nakedlittlezombie@users.noreply.github.com>
2026-09-08 09:49:57 +0200

    requirements

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 git update-index --skip-worktree docker-compose.override.yml
+git update-index --skip-worktree requirements.txt
 git pull
 docker compose down
 docker compose up -d --build
